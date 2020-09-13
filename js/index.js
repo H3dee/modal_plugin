5e59bddb8f42e55ad77f230790ea7e270c9661d5
H3dee <62655938+H3dee@users.noreply.github.com>
2020-09-13 13:48:30 +0300

    v2.0

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-// $.modal()
+const modal =  $.modal({
+    title: 'Modal window',
+    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui modi accusantium officia eveniet blanditiis quod, laudantium soluta, asperiores eligendi, repudiandae possimus beatae pariatur est autem deserunt veniam eos voluptate iste?',
+    closable: true,
+    width: '500px'
+})
--- modal/modal.js
+++ modal/modal.js
@@ -1,37 +1,138 @@
-const _createModal = options => {
+
+const _createCards = data => {
+  
+}
+
+const _createModal = (options) => {
+  const defaultValues = {
+    TITLE: "default title",
+    CONTENT: "default content",
+    WIDTH: "500px",
+  };
+  //creating modal-container
   const modal = document.createElement("div");
+  //filling modal-container
   modal.insertAdjacentHTML(
     "afterbegin",
-    `<div class="modal__overlay">
-      <div class="modal__window">
+    `<div class="modal__overlay" data-closable=' '>
+      <div 
+      class="modal__window"
+      style='width: ${options.width || defaultValues.WIDTH}'
+      >
         <div class="modal__header">
-          <div class="modal__title">Modal header</div>
-          <div class="modal__close">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/OOjs_UI_icon_close.svg/1200px-OOjs_UI_icon_close.svg.png"
-              alt=" "
-            />
-          </div>
+          <div class="modal__title">${
+            options.title || defaultValues.TITLE
+          }</div>
+          ${
+            options.closable
+              ? `<div class="modal__close">
+          <img
+            data-closable='close__icon'
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/OOjs_UI_icon_close.svg/1200px-OOjs_UI_icon_close.svg.png"
+            alt=" "
+          />
+        </div>`
+              : `<div></div>`
+          }
         </div>
         <div class="modal__body">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, ducimus.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, ducimus.</p>
+            <div class='modal__content'>
+              ${options.content || defaultValues.CONTENT}
+            </div>
         </div>
         <div class="modal__footer">
-              <button>ok</button>
-              <button>cancel</button>
+              <div class='modal__buttons'>
+                <button data-closable='closeBtn'>Close</button>
+                <button id='saveBtn'>Save Changes</button>
+              </div>
         </div>
       </div>`
   );
-  modal.classList.add('modal')
-  return modal
+  modal.classList.add("modal");
+  document.body.appendChild(modal);
+  return modal;
 };
 
+/**
+ *params:
+ *title: string, //done
+ *closable: boolean, //done
+ *content: string, //done
+ *width: string, //done
+ *destroy(): remove listeners & content, //done
+ *closeWindow() //done
+ *-----------------
+ *setContent(html:string): void | Public, //done
+ *lifecycle's methods: onOpen(), onClose(), beforeClose(): boolean //done
+ */
+
 $.modal = function (options) {
-  const $modal = _createModal(options)
+  const $modal = _createModal(options);
+  const ANIMATION_SPEED = 350;
+  const modalContent = $modal.textContent;
+  let closing = false;
+  let isDestroyed = false;
+
+  function open() {
+    if (isDestroyed) console.log("Modal is destroyed");
+    else {
+      !closing && $modal.classList.add("open");
+      onOpen();
+
+      $modal.addEventListener("click", handleOverlayClick);
+    }
+  }
+
+  function onOpen() {
+    console.log("Modal opened!");
+  }
+
+  function onClose() {
+    alert("Modal Closed");
+  }
+
+  function shouldModalClose() {
+    if (modalContent.length >= 450) return true;
+    else return false;
+  }
+
+  function alertContent() {
+    alert(modalContent);
+  }
+
+  function handleOverlayClick(event) {
+    if (shouldModalClose()) {
+      const et = event.target;
+      if(et.dataset.closable) close()
+      else if(et.getAttribute('id') === 'saveBtn') alertContent()
+    }
+  }
+
+  function setContent(newContent) {
+    modalContent.textContent = String(newContent);
+  }
+
+  function close() {
+    closing = true;
+    $modal.classList.remove("open");
+    $modal.classList.add("close");
+    setTimeout(() => {
+      $modal.classList.remove("close");
+      closing = false;
+      onClose();
+    }, ANIMATION_SPEED);
+  }
+
+  function destroy() {
+    isDestroyed = true;
+    $modal.removeEventListener("click", handleOverlayClick);
+    $modal.remove();
+  }
+
   return {
-    open() {},
-    close() {},
-    destroy() {},
+    open,
+    close,
+    destroy,
+    setContent,
   };
 };
