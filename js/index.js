9afa55bd69a6271328c1cda95149c1457a0ead4c
H3dee <62655938+H3dee@users.noreply.github.com>
2020-09-15 13:56:01 +0300

    released

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
+const cards = [
+    {
+        id: '1',
+        subtitle: 'Яблоко',
+        configurable: true,
+        price: '50$/kg',
+        imgUrl: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'
+    },
+    {
+        id: '2',
+        price: '80$/kg',
+        subtitle: 'Апельсин',
+        configurable: true,
+        imgUrl: 'https://m.dom-eda.com/uploads/images/catalog/item/dfc9a3e974/3cbf3bd41c_1000.jpg'
+    },
+    {
+        id: '3',
+        price: '120$/kg',
+        subtitle: 'Манго',
+        configurable: true,
+        imgUrl: 'https://fruit-berries.ru/images/cms/thumbs/1f1204c38f5d7f50f0ab6bcf597ef97666ee60e8/mango-brazilia_483_auto_jpg_5_92.jpg'
+    }
+]
+
 const modal =  $.modal({
     title: 'Modal window',
     content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui modi accusantium officia eveniet blanditiis quod, laudantium soluta, asperiores eligendi, repudiandae possimus beatae pariatur est autem deserunt veniam eos voluptate iste?',
     closable: true,
-    width: '500px'
+    width: '500px',
+    cards
 })
--- modal/modal.js
+++ modal/modal.js
@@ -1,7 +1,59 @@
+const _createCards = (selector, data) => {
+  const container = document.querySelector(selector);
+  const cards = data.reduce((acc, item) => {
+    return (
+      acc +
+      `<div class="fruits__card" data-id=${item.id}>
+    <div class="card__image">
+      <img src=${
+        item.imgUrl ||
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAABlBMVEUKCgoKCwYm8ZtEAAAAY0lEQVR4nO3csREAIAgEQb7/pm1BTBRntwLmoo+oAgAAAIBBktsXDBK19mkFAAAfMvQbxGrQCgAAAAAAAAAAAAAAAAAAAAAAOOeTEQAAAAAAAAAAAAAAAAAAAAAAAAAAAPC6BSuuAA7szvsXAAAAAElFTkSuQmCC"
+      } alt=" ">
+    </div> 
+    <div class="card__info">
+      <div class="card__subtitle">
+        ${item.subtitle || "Default"}
+      </div>
+      <div class="card__btns" data-id=${item.id}>
+        <button id='priceBtn' data-card__btn=' '>
+          Посмотреть цену
+        </button>
+        <!-- <button id='removeBtn' data-card__btn=' '>
+          Удалить
+        </button> --> 
+      </div>
+    </div>
+</div>`
+    );
+  }, ``);
+  container.innerHTML = ` <h1>Header</h1>
+  <div class="container">
+    <div class="content__row">
+      <div class="content__body">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
+        expedita vitae ut dolor? Veritatis consectetur fugiat facilis cupiditate
+        placeat magni dicta ipsum, maiores odio autem aut! Dolore, aut.
+        Pariatur, dolores?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
+        expedita vitae ut dolor? Veritatis consectetur fugiat facilis cupiditate
+        placeat magni dicta ipsum, maiores odio autem aut! Dolore, aut.
+        Pariatur, dolores?
+      </div>
+      <div class="content__fruits">
+        <div class="fruits__title">
+          Fruits
+        </div>
+        <div class="container">
+          <div class="fruits__row">
+          ${cards}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+};
 
-const _createCards = data => {
-  
-}
+const generateRandomId = (min = 1, max = 100) => Math.round(Math.random() * (max - min) + min)
 
 const _createModal = (options) => {
   const defaultValues = {
@@ -49,30 +101,57 @@ const _createModal = (options) => {
       </div>`
   );
   modal.classList.add("modal");
+  _createCards(`[data-container='content'`, options.cards);
   document.body.appendChild(modal);
   return modal;
 };
 
-/**
- *params:
- *title: string, //done
- *closable: boolean, //done
- *content: string, //done
- *width: string, //done
- *destroy(): remove listeners & content, //done
- *closeWindow() //done
- *-----------------
- *setContent(html:string): void | Public, //done
- *lifecycle's methods: onOpen(), onClose(), beforeClose(): boolean //done
- */
-
 $.modal = function (options) {
-  const $modal = _createModal(options);
+  let $modal = _createModal(options);
+  const $cardContainer = document.querySelector(".fruits__row");
   const ANIMATION_SPEED = 350;
-  const modalContent = $modal.textContent;
+  const modalContent = document.querySelector('.modal__content');
+  let newId = 1
   let closing = false;
   let isDestroyed = false;
 
+  //Set listener to cards' parent
+  $cardContainer.addEventListener("click", handleBtnClick);
+
+  //card's button handler
+  function handleBtnClick(event) {
+    const et = event.target;
+    const id = et.closest(".card__btns").dataset.id;
+
+    //Check price of clicked card
+    if(et.dataset.card__btn && (et.getAttribute('id') === 'priceBtn')) {
+      console.log('check')
+      modal.setContent(`
+        <p>
+          <span>
+            Price:
+          </span>
+          ${options.cards[+(id) - 1].price}
+        </p>
+        <p><span>Вид фрукта:</span> ${options.cards[+id - 1].subtitle} </p>
+      `)
+      open();
+    }
+  
+    //Remove card
+    else if(et.dataset.card__btn && (et.getAttribute('id') === 'removeBtn')){
+      options.cards = options.cards
+                             .filter((item, index) =>  index !== (+id - 1))
+      options.cards.forEach(item => {
+        item.id = newId
+        newId++
+      });
+              
+      _createCards(`[data-container='content']`, options.cards)
+    }
+  }
+
+  //open modal
   function open() {
     if (isDestroyed) console.log("Modal is destroyed");
     else {
@@ -83,35 +162,43 @@ $.modal = function (options) {
     }
   }
 
+  //called after modal was opend
   function onOpen() {
     console.log("Modal opened!");
   }
 
+  //called after modal was closed
   function onClose() {
     alert("Modal Closed");
   }
 
+  //modal can be closed when method returns true
   function shouldModalClose() {
-    if (modalContent.length >= 450) return true;
-    else return false;
+    // if (modalContent.textContent.length >= 100) return true;
+    // else return false;
+    return true;
   }
 
+  //handle check button of modal
   function alertContent() {
-    alert(modalContent);
+    alert('saved');
   }
 
+  //handle click out of modal
   function handleOverlayClick(event) {
     if (shouldModalClose()) {
       const et = event.target;
-      if(et.dataset.closable) close()
-      else if(et.getAttribute('id') === 'saveBtn') alertContent()
+      if (et.dataset.closable) close();
+      else if (et.getAttribute("id") === "saveBtn") alertContent();
     }
   }
 
-  function setContent(newContent) {
-    modalContent.textContent = String(newContent);
+  //change content of modal
+  function setContent(html) {
+    modalContent.innerHTML = html;
   }
 
+  //close modal
   function close() {
     closing = true;
     $modal.classList.remove("open");
@@ -123,10 +210,11 @@ $.modal = function (options) {
     }, ANIMATION_SPEED);
   }
 
+  //total removing of modal
   function destroy() {
     isDestroyed = true;
     $modal.removeEventListener("click", handleOverlayClick);
-    $modal.remove();
+    $modal.parentNode.removeChild($modal);
   }
 
   return {
